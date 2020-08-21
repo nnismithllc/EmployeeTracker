4ac5ddfac2434980aad7f58395eb9923f73e9a15
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-08-20 21:07:01 -0400

    updated classes per member

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+constructor (name, id, email){
+    this.name= employeeName;
+    this.id= employeeID;
+    this.email= employeeEmail;
+}
+
+Employee.getEmplyname () {
+    return this.name;
+}
+
+Employee.getEmplyid () {
+    return this.id;
+}
+
+Employee.getEmplyemail ();
+
+
+
+
+
+
+
+}
--- Develop/templates/app.js
+++ Develop/templates/app.js
@@ -13,6 +13,33 @@ const render = require("./lib/htmlRenderer");
 const Choice = require("inquirer/lib/objects/choice");
 
 
+var PORT = 3000;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+  var path = req.url;
+
+  switch(path.uChoice) {
+    case "Manager":
+    addManager();
+    break;
+    case "Engineer":
+    addEngineer();
+    break;
+    case "Employee":
+    addEmployee();
+    break;
+    case "Intern":
+    addIntern();
+    break;
+    default:
+}
+
+
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 const teamMembers =[];
 const idArray = [];
@@ -56,7 +83,7 @@ function createTeam(){
 function createManager() {
 inquirer.prompt([
 {
-    type:"Choice",
+    type:"choice",
     name: ["Yes", "No"],
     message: "Are you the Manager?"
 },
@@ -112,7 +139,7 @@ function addEmployee(){
     message: "What is the Employee Email Address?"
 },          
 {
-    type:"Choice",
+    type:"choice",
     name: ["Yes", "No"],
     message: "Would You Like to Add Another Member?"
 },         
@@ -138,17 +165,17 @@ function addEngineer(){
     message: "Please enter the six digit Employee ID number."
 },
 {
-    type:"Input",
+    type:"input",
     name: "Email",
     message: "What is the Employee Email Address?"
 }, 
 {
-    type:"Input",
+    type:"input",
     name: "GitHub",
     message: "What is the Employee GitHub Username?"
 },                   
 {
-    type:"Choice",
+    type:"choice",
     name: ["Yes", "No"],
     message: "Would You Like to Add Another Member?"
 },         
@@ -196,28 +223,15 @@ createEngineer();{
 
 }
 
-
 addTeam();
 
-// After the user has input all employees desired, call the `render` function (required
+// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
 
 
+// Hint: you may need to check if the `output` folder exists and create it if it does not.
 
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! 
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work!
