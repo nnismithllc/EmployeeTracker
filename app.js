d154a8283f3c25e7d6043db840d74f0a75f3b574
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-08-21 15:14:12 -0400

    changed the location of the app.js file

--- app.js
+++ app.js
@@ -6,57 +6,78 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const outputPath_DIR = path.resolve(__dirname, "output");
+const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Choice = require("inquirer/lib/objects/choice");
-
+// const choice = require("inquirer/lib/objects/choice");
 
 var PORT = 3000;
-
-var server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  var path = req.url;
-
-  switch(path.uChoice) {
-    case "Manager":
-    addManager();
-    break;
-    case "Engineer":
-    addEngineer();
-    break;
-    case "Employee":
-    addEmployee();
-    break;
-    case "Intern":
-    addIntern();
-    break;
-    default:
-}
-
-
-
-
 
 // Write code to use inquirer to gather information about the development team members,
 const teamMembers =[];
 const idArray = [];
 
-function addTeam(){
+function appTeam(){
+
+    function createManager (){
+        inquirer.prompt([
+        {
+            type:"choice",
+            name: ["Yes", "No"],
+            message: "Are you the Manager?",
+            validate: answer => {
+                if (answer !== ""){
+                return true;
+            }
+            return "Please have Manager enter information.";
+        }
+        },
+        {
+            type:"Input",
+            name: "name",
+            message: "What is Your Name?",
+            validate: answer => {
+                if (answer !== ""){
+                return true;
+            }
+            return "Please enter at least 2 characters.";
+        }
+        },
+        {
+            type:"Input",
+            name: "office",
+            message: "What is Your Office Branch Number?"
+        },
+        {
+            type:"Input",
+            name: "id",
+            message: "What is Your Employee ID Number?"
+        },
+        {
+            type:"Input",
+            name: "email",
+            message: "What is Your Employee Email Address?"
+        },
+        {
+            type:"Input",
+            name: "employee",
+            message: "Please add a member to your team."
+        }  
+        ])
+        }
 
 function createTeam(){
     inquirer.prompt([
         {
             type:"choice",
-            name: ["Manager", "Engineer", "Employee", "Intern"],
+            name: "memberChoice",
+            choices:["Manager", "Engineer", "Employee", "Intern"],
             message: "What Type of Member do You want to add?"
         }
-
         
-        ]).then(uSelection =>{
-         switch(uSelection.uChoice) {
+        ]).then(uSelection => {
+         switch(uSelection.memberChoice) {
              case "Manager":
              addManager();
              break;
@@ -70,51 +91,12 @@ function createTeam(){
              addIntern();
              break;
              default:
+                 buildTeam ();
          }
+        })
 
-
-        }
-
-
-
-        )
+        
     }
-
-function createManager() {
-inquirer.prompt([
-{
-    type:"choice",
-    name: ["Yes", "No"],
-    message: "Are you the Manager?"
-},
-{
-    type:"Input",
-    name: "Manager",
-    message: "What is Your Name?"
-},
-{
-    type:"Input",
-    name: "Office",
-    message: "What is Your Office Branch Number?"
-},
-{
-    type:"Input",
-    name: "EmployeeID",
-    message: "What is Your Employee ID Number?"
-},
-{
-    type:"Input",
-    name: "Email",
-    message: "What is Your Employee Email Address?"
-},
-{
-    type:"Input",
-    name: "Employee",
-    message: "Please add a member to your team."
-}  
-
-])
-}
 
 function addEmployee(){
     inquirer.prompt([
@@ -125,17 +107,17 @@ function addEmployee(){
 },
 {
     type:"input",
-    name: "EmployeeName",
+    name: "employeeName",
     message: "Please enter the First and Last Name."
 },
 {
     type:"input",
-    name: "EmployeeID",
+    name: "employeeID",
     message: "Please enter the six digit Employee ID number."
 },
 {
     type:"Input",
-    name: "Email",
+    name: "employeeEmail",
     message: "What is the Employee Email Address?"
 },          
 {
@@ -148,9 +130,15 @@ function addEmployee(){
     type:"choice",
     name: ["Engineer","Employee", "Intern","Manager"],
     message: "What is the Employment Title?"
-}
-])
+},
 
+]).then(answers => {
+    const employee = new Employee (answers.employeeName, answers.employeeID, answers.employeeEmail)
+    teamMembers.push(employee);
+    idArray.push(answers.employeeId);
+    createTeam ();
+})
+}
 function addEngineer(){
     inquirer.prompt([
 
@@ -179,8 +167,14 @@ function addEngineer(){
     name: ["Yes", "No"],
     message: "Would You Like to Add Another Member?"
 },         
-])
+]).then(answers => {
+    const engineer = new Engineer (answers.engineerName, answers.engineerID, answers.engineerEmail)
+    teamMembers.push(engineer);
+    idArray.push(answers.engineerId);
+    createTeam ();
+})
 
+}
 function addIntern(){
 inquirer.prompt([
 {
@@ -209,23 +203,27 @@ inquirer.prompt([
     message: "Would You Like to Add Another Member?"
 },         
 ])
+.then(answers => {
+const intern = new Intern (answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+teamMembers.push(intern);
+idArray.push(answers.internId);
+createTeam ();
+});
 
+}
 
 function buildTeam(){
-fs.writeFileSync(outputPath, render(teamMembers),"utf-8S")
+fs.writeFileSync(outputPath, render(teamMembers),"utf-8s");
 }
 
-createManager();{
-
-}
-
-createEngineer();{
+createManager();
 
 }
 
-addTeam();
 
-// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
+appTeam();
+
+// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated div for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
 
